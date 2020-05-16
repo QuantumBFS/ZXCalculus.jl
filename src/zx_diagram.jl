0fd3e5b53dcfabdf0e6e2d11f6c964cbc47272e9
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2020-05-16 21:32:51 +0800

    change storage to Dict

--- src/zx_diagram.jl
+++ src/zx_diagram.jl
@@ -1,4 +1,4 @@
-using LightGraphs, Multigraphs
+using LightGraphs
 import Base: show, copy
 import LightGraphs: nv, ne, outneighbors, rem_edge!
 
@@ -6,15 +6,15 @@ export ZXDiagram, SType, Z, X, H, In, Out
 
 @enum SType Z X H In Out
 
-mutable struct ZXDiagram{T<:Integer, U<:Integer, P}
-    g::Multigraph{T,U}
+mutable struct ZXDiagram{T<:Integer, P}
+    mg::Multigraph{T}
 
-    st::Vector{SType}
-    ps::Vector{P}
+    st::Dict{T, SType}
+    ps::Dict{T, P}
 
-    function ZXDiagram(g::Multigraph{T, U}, st::Vector{SType}, ps::Vector{P}) where {T<:Integer, U<:Integer, P}
-        if nv(g) == length(ps) && nv(g) == length(st)
-            zxd = new{T, U, P}(g, st, ps)
+    function ZXDiagram{T, P}(mg::Multigraph{T}, st::Dict{T, SType}, ps::Dict{T, P}) where {T<:Integer, P}
+        if nv(mg) == length(ps) && nv(mg) == length(st)
+            zxd = new{T, P}(mg, st, ps)
             rounding_phases!(zxd)
             return zxd
         else
@@ -23,11 +23,16 @@ mutable struct ZXDiagram{T<:Integer, U<:Integer, P}
     end
 end
 
-copy(zxd::ZXDiagram) = ZXDiagram(copy(zxd.g), copy(zxd.st), copy(zxd.ps))
+ZXDiagram(mg::Multigraph{T}, st::Dict{T, SType},
+    ps::Dict{T, P}) where {T, P} = ZXDiagram{T, P}(mg, st, ps)
+ZXDiagram(mg::Multigraph{T}, st::Vector{SType},
+    ps::Vector{P}) where {T, P} =
+    ZXDiagram(mg, Dict(zip(vertices(mg), st)), Dict(zip(vertices(mg), ps)))
 
-spider_type(zxd::ZXDiagram{T, U, P}, v::T) where {T<:Integer, U<:Integer, P} = zxd.st[v]
+copy(zxd::ZXDiagram) = ZXDiagram(copy(zxd.mg), copy(zxd.st), copy(zxd.ps))
+spider_type(zxd::ZXDiagram{T, P}, v::T) where {T<:Integer, P} = zxd.st[v]
 
-function print_spider(io::IO, zxd::ZXDiagram{T, U, P}, v::T) where {T<:Integer, U<:Integer, P}
+function print_spider(io::IO, zxd::ZXDiagram{T, P}, v::T) where {T<:Integer, P}
     st_v = spider_type(zxd, v)
     if st_v == Z
         printstyled(io, "S_$(v){phase = $(zxd.ps[v])⋅π}"; color = :green)
@@ -42,14 +47,14 @@ function print_spider(io::IO, zxd::ZXDiagram{T, U, P}, v::T) where {T<:Integer, 
     end
 end
 
-function show(io::IO, zxd::ZXDiagram{T, U, P}) where {T<:Integer, U<:Integer, P}
-    println(io, "ZX-diagram with $(nv(zxd.g)) vertices and $(ne(zxd.g)) multiple edges:")
-    for v1 in vertices(zxd.g)
-        for v2 in outneighbors(zxd.g, v1)
+function show(io::IO, zxd::ZXDiagram{T, P}) where {T<:Integer, P}
+    println(io, "ZX-diagram with $(nv(zxd.mg)) vertices and $(ne(zxd.mg)) multiple edges:")
+    for v1 in vertices(zxd.mg)
+        for v2 in outneighbors(zxd.mg, v1)
             if v2 >= v1
                 print(io, "(")
                 print_spider(io, zxd, v1)
-                print(io, " <-$(zxd.g.adjmx[v2, v1])-> ")
+                print(io, " <-$(mul(zxd.mg, v1, v2))-> ")
                 print_spider(io, zxd, v2)
                 print(io, ")\n")
             end
@@ -57,46 +62,53 @@ function show(io::IO, zxd::ZXDiagram{T, U, P}) where {T<:Integer, U<:Integer, P}
     end
 end
 
-nv(zxd::ZXDiagram) = nv(zxd.g)
-ne(zxd::ZXDiagram, count_mul::Bool = false) = ne(zxd.g, count_mul)
-outneighbors(zxd::ZXDiagram, v) = outneighbors(zxd.g, v)
+nv(zxd::ZXDiagram) = nv(zxd.mg)
+ne(zxd::ZXDiagram, count_mul::Bool = false) = ne(zxd.mg, count_mul)
+outneighbors(zxd::ZXDiagram, v) = outneighbors(zxd.mg, v)
 function rem_edge!(zxd::ZXDiagram, x...)
-    rem_edge!(zxd.g, x...)
-    zxd
+    rem_edge!(zxd.mg, x...)
 end
 
-function rem_spiders!(zxd::ZXDiagram{T,U,P}, vs::Vector{T}) where {T<:Integer, U<:Integer, P}
-    vmap = rem_vertices!(zxd.g, vs)
-    deleteat!(zxd.ps, sort(vs))
-    deleteat!(zxd.st, sort(vs))
-    return vmap
+function rem_spiders!(zxd::ZXDiagram{T, P}, vs::Vector{T}) where {T<:Integer, P}
+    if rem_vertices!(zxd.mg, vs)
+        for v in vs
+            delete!(zxd.ps, v)
+            delete!(zxd.st, v)
+        end
+        return true
+    end
+    return false
 end
-rem_spider!(zxd::ZXDiagram{T,U,P}, v::T) where {T<:Integer, U<:Integer, P} = rem_spiders!(zxd, [v])
+rem_spider!(zxd::ZXDiagram{T, P}, v::T) where {T<:Integer, P} = rem_spiders!(zxd, [v])
 
-function add_spider!(zxd::ZXDiagram{T,U,P}, st::SType, phase::P, connect::Vector{T}=T[]) where {T<:Integer, U<:Integer, P}
-    add_vertex!(zxd.g)
-    v = nv(zxd.g)
-    push!(zxd.ps, phase)
-    push!(zxd.st, st)
+function add_spider!(zxd::ZXDiagram{T, P}, st::SType, phase::P, connect::Vector{T}=T[]) where {T<:Integer, P}
+    add_vertex!(zxd.mg)
+    v = vertices(zxd.mg)[end]
+    zxd.ps[v] = phase
+    zxd.st[v] = st
+    connect ⊆ vertices(zxd.mg)
     for c in connect
-        add_edge!(zxd.g, v, c)
+        add_edge!(zxd.mg, v, c)
     end
     zxd
 end
 
-function insert_spider!(zxd::ZXDiagram{T,U,P}, v1::T, v2::T, st::SType, phase::P = zero(P)) where {T<:Integer, U<:Integer, P}
-    for i = 1:zxd.g.adjmx[v1,v2]
+function insert_spider!(zxd::ZXDiagram{T, P}, v1::T, v2::T, st::SType, phase::P = zero(P)) where {T<:Integer, P}
+    for i = 1:mul(zxd.mg, v1, v2)
         add_spider!(zxd, st, phase, [v1, v2])
         rem_edge!(zxd, v1, v2)
     end
     zxd
 end
 
-function rounding_phases!(zxd::ZXDiagram{T,U,P}) where {T<:Integer, U<:Integer, P}
+function rounding_phases!(zxd::ZXDiagram{T, P}) where {T<:Integer, P}
     ps = zxd.ps
-    ps .+= one(P)
-    ps = rem.(ps, one(P)+one(P))
-    ps .-= one(P)
+    for (v, p) in ps
+        p += one(P)
+        p = rem(p, one(P)+one(P))
+        p -= one(P)
+        ps[v] = p
+    end
     zxd.ps = ps
 end
 
