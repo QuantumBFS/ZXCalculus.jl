9b1dcc76c46b874775ead930c274603656acb4a1
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2020-06-27 10:58:59 +0800

    code cov for MultipleEdge

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,8 @@ using Test
     end
     @test src(me) == 1 && dst(me) == 2 && mul(me) == 3
     e0 = LightGraphs.SimpleEdge(me)
+    MultipleEdge(e0)
+    @test MultipleEdge(1, 2) == e0
     @test e0 == MultipleEdge(1, 2)
     @test e0 == MultipleEdge([1, 2])
     @test e0 == MultipleEdge([1, 2, 1])
@@ -20,6 +22,7 @@ using Test
     @test reverse(me) == MultipleEdge(2, 1, 3)
     @test eltype(me) == Int
 
+    @test iterate(me)[2] == 2
     @test [e0 == e for e in me] == [true for i = 1:mul(me)]
     @test Tuple(me) == (1,2,3)
     length(me) == mul(me)
