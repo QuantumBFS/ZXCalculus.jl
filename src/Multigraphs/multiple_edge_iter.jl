dc5b0070322ceb7845d503c9e5b855ee7b982083
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2020-06-27 11:06:39 +0800

    remove redundant

--- src/Multigraphs/multiple_edge_iter.jl
+++ src/Multigraphs/multiple_edge_iter.jl
@@ -10,38 +10,6 @@ end
 
 eltype(::Type{MultipleEdgeIter{AbstractMultigraph{T}}}) where {T<:Integer} = MultipleEdge{T, Int}
 
-function iterate(eit::MultipleEdgeIter{G}, state=(one(eltype(eit.g)), one(eltype(eit.g)))) where {G <: AbstractMultigraph}
-    g = eit.g
-    n = nv(g)
-    u, i = state
-
-    @inbounds while u <= n
-        list_u = outneighbors(g, u)
-        if i > length(list_u)
-            u += 1
-            i = one(u)
-            continue
-        end
-        if is_directed(g)
-            e = MultipleEdge(u, list_u[i], g.adjmx[u, list_u[i]])
-            state = (u, i + 1)
-            return e, state
-        else
-            if list_u[i] >= u
-                e = MultipleEdge(u, list_u[i], g.adjmx[list_u[i], u])
-                state = (u, i + 1)
-                return e, state
-            else
-                i += 1
-            end
-        end
-    end
-
-    if n == 0 || u > n
-        return nothing
-    end
-end
-
 function iterate(eit::MultipleEdgeIter{G}, state=(one(eltype(eit.g)), one(eltype(eit.g)))) where {G <: Multigraph}
     g = eit.g
     n = nv(g)
