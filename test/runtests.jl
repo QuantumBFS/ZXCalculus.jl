b10d69eba63f43572880c6ea77e63334534cc5d6
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2020-06-27 10:38:37 +0800

    add test for Multigraph

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,110 @@
 using ZXCalculus
+using LightGraphs, SparseArrays
 using Test
-using LightGraphs
+
+@testset "multiple_edge.jl" begin
+    me = MultipleEdge(1, 2, 3)
+    try
+        MultipleEdge(1, 2, 0)
+    catch err
+        @test err != nothing
+    end
+    @test src(me) == 1 && dst(me) == 2 && mul(me) == 3
+    e0 = LightGraphs.SimpleEdge(me)
+    @test e0 == MultipleEdge(1, 2)
+    @test e0 == MultipleEdge([1, 2])
+    @test e0 == MultipleEdge([1, 2, 1])
+    @test e0 == MultipleEdge((1, 2))
+    @test e0 == MultipleEdge((1, 2, 1))
+    @test e0 == MultipleEdge(1 => 2)
+    @test reverse(me) == MultipleEdge(2, 1, 3)
+    @test eltype(me) == Int
+
+    @test [e0 == e for e in me] == [true for i = 1:mul(me)]
+    @test Tuple(me) == (1,2,3)
+    length(me) == mul(me)
+end
+
+@testset "multigraph_adjlist.jl" begin
+    try
+        m2 = spzeros(Int, 2, 3)
+        dg = Multigraph(m2)
+    catch e
+        @test e != nothing
+    end
+    try
+        m2 = spzeros(Int, 2, 2)
+        m2[1, 2] = 2
+        dg = Multigraph(m2)
+    catch e
+        @test e != nothing
+    end
+    try
+        m2 = spzeros(Int, 2, 2)
+        m2[1, 2] = -1
+        m2[2, 1] = -1
+        dg = Multigraph(m2)
+    catch e
+        @test e != nothing
+    end
+
+    m = spzeros(Int, 4, 4)
+    m
+    m[1,2] = 2
+    m[2,1] = 2
+    m[2,3] = 2
+    m[3,2] = 2
+    m[3,4] += 1
+    m[3,4] = 0
+    m[4,3] += 1
+    m[4,3] = 0
+    g = Multigraph(m)
+    g = Multigraph(Matrix(m))
+
+    g0 = Multigraph(2)
+    @test !add_edge!(g0, 2, 3) && !rem_edge!(g0, 1, 2)
+    g1 = Multigraph(path_graph(3))
+
+    @test !is_directed(g)
+    @test edgetype(g) == MultipleEdge{Int, Int}
+    @test size(adjacency_matrix(g), 1) == 4
+
+    @test nv(g) == 4 && ne(g, count_mul = true) == 4 && ne(g) == 2
+
+    add_vertices!(g, 3)
+    @test nv(g) == 7
+
+    @test has_edge(g, 1, 2, 2)
+    @test rem_vertices!(g, [7, 5, 4, 6])
+    add_edge!(g, [2, 3, 2])
+    rem_edge!(g, [2, 3, 2])
+    add_edge!(g, 2, 3)
+    rem_edge!(g, 2, 3)
+    add_edge!(g, 2, 3, 2)
+    rem_edge!(g, 2, 3, 1)
+
+    @test has_edge(g, 2, 3) && has_edge(g, [2, 3])
+    @test has_edge(g, 2, 3, 2) && has_edge(g, (2, 3, 2))
+    @test !has_edge(g, 2, 2) && !has_edge(g, 2, 5)
+    @test has_vertex(g, 1) && !has_vertex(g, 5)
+    for v in vertices(g)
+        @test inneighbors(g, v) == outneighbors(g, v)
+        @test degree(g, v) == indegree(g, v) && indegree(g, v) == outdegree(g, v)
+    end
+    add_vertex!(g)
+end
+
+@testset "multiple_edge_iter.jl" begin
+    mg = Multigraph(3)
+    add_vertices!(mg, 3)
+    rem_vertices!(mg, [1, 3])
+    add_edge!(mg, 2, 5)
+    add_edge!(mg, 2, 4, 2)
+
+    @test outneighbors(mg, 2) == [4, 5]
+    mes = [me for me in edges(mg)]
+    @test length(mes) == 2
+end
 
 # include("../script/zx_plot.jl")
 
