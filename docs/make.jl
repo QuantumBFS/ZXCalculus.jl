dd56cc2be92092910402020cc544095e7805cc49
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2020-07-17 14:48:08 +0800

    typos

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ makedocs(;
     modules = [ZXCalculus],
     format = Documenter.HTML(prettyurls = !("local" in ARGS)),
     pages = ["Home" => "index.md",
-        "Tutorials" => "tutorial.md",
+        "Tutorial" => "tutorial.md",
         "APIs" => "api.md"
     ],
     repo = "https:/github.com/QuantumBFS/ZXCalculus.jl",
