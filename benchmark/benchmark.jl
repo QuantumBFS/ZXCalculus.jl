cd2d6ad335a30796857b1516a93a48ed0a1968b3
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2020-08-09 20:40:05 +0800

    add benchmarks

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -1,15 +1,25 @@
 using YaoLang: YaoIR, is_pure_quantum
 using ZXCalculus
-circ_name = "adder_8"
-srcs = readlines("benchmark/circuits/$(circ_name).qasm")
-src = prod([srcs[1]; srcs[3:end]])
-m = @__MODULE__
-ir = YaoIR(m, src, circ_name)
-ir.pure_quantum = true
-zxd = ZXDiagram(ir);
-using ProfileView: @profview
-@profiler ZXGraph(zxd);
-@profview phase_teleportation(zxd)
 using BenchmarkTools
-@benchmark clifford_simplification(zxd)
-@benchmark phase_teleportation(zxd);
+
+function zx_load_qasm(filename::String)
+    srcs = readlines("benchmark/circuits/$(filename)")
+    src = prod([srcs[1]; srcs[3:end]])
+    m = @__MODULE__
+    ir = YaoIR(m, src, "qasm_circ")
+    ir.pure_quantum = is_pure_quantum(ir)
+    return zxd = ZXDiagram(ir)
+end
+function run_benchmark()
+    filenames = readdir("benchmark/circuits")
+    bms = []
+    for circ_name in filenames
+        zxd = zx_load_qasm(circ_name)
+        b = @benchmark phase_teleportation($zxd)
+        println(circ_name, "\t time = ", (mean(b).time / 1e9))
+        push!(bms, b)
+    end
+    return Dict(zip(filenames, bms))
+end
+
+bms = run_benchmark()
