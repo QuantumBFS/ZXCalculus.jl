715bb78540ad2daa8b9c2640075acb891914a5d8
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2020-08-31 12:49:39 +0800

    update benchmarks.jl

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -6,20 +6,20 @@ function zx_load_qasm(filename::String)
     srcs = readlines("benchmark/circuits/$(filename)")
     src = prod([srcs[1]; srcs[3:end]])
     m = @__MODULE__
-    ir = YaoIR(m, src, "qasm_circ")
+    ir = YaoIR(m, src, gensym())
     ir.pure_quantum = is_pure_quantum(ir)
     return zxd = ZXDiagram(ir)
 end
 function run_benchmark()
     filenames = readdir("benchmark/circuits")
-    bms = []
+    bms = Dict()
     for circ_name in filenames
         zxd = zx_load_qasm(circ_name)
         b = @benchmark phase_teleportation($zxd)
-        println(circ_name, "\t time = ", (mean(b).time / 1e9))
-        push!(bms, b)
+        println(circ_name, "\ttime = ", (mean(b).time / 1e9), "\ttcount = ", tcount(phase_teleportation(zxd)))
+        bms[circ_name] = b
     end
-    return Dict(zip(filenames, bms))
+    bms
 end
 
 bms = run_benchmark()
