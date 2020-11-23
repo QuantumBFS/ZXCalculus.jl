4db9eb5e72397c08800548cb60198e51224b942c
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2020-11-23 16:40:12 +0800

    rework for `Phase`

--- src/phase.jl
+++ src/phase.jl
@@ -1,72 +1,86 @@
-import Base: +, -, *, ==, isless, rem, convert, zero, one, iseven
+import Base: +, -, *, /, ==, isless, rem, convert, zero, one, iseven
 import Base: show
 
 """
     Phase
 The type supports manipulating phases as expressions.
 """
-struct Phase
+struct Phase{T}
     ex
-    val
 end
 
-Phase(p::Real) = Phase(nothing, p)
-Phase(p::Expr) = Phase(p, 0//1)
-Phase(p::Symbol) = Phase(p, 0//1)
+Phase(p::T) where {T} = Phase{T}(p)
+
 Phase(p::Phase) = p
 
 function show(io::IO, p::Phase)
-    if isnothing(p.ex)
-        print(io, p.val)
-    else
-        print(io, p.ex, " + ", p.val)
-    end
+    print(io, p.ex)
 end
 
-function +(p1::Phase, p2::Phase)
-    if isnothing(p1.ex)
-        return Phase(p2.ex, p1.val + p2.val)
-    elseif isnothing(p2.ex)
-        return Phase(p1.ex, p1.val + p2.val)
-    else
-        return Phase(:($(p1.ex) + $(p2.ex)), p1.val + p2.val)
+function +(p1::Phase{T1}, p2::Phase{T2}) where {T1, T2}
+    if p1.ex isa Number && p2.ex isa Number
+        return Phase(p1.ex + p2.ex)
     end
-end
-+(p1::Phase, p2::Real) = Phase(p1.ex, p1.val + p2)
-+(p1::Real, p2::Phase) = Phase(p1.val + p2, p2.ex)
 
-function -(p::Phase)
-    if isnothing(p.ex)
-        return Phase(nothing, -p.val)
-    else
-        return Phase(:(-$(p.ex)), -p.val)
-    end
+    T = Base.promote_op(+, T1, T2)
+    return Phase{T}(Expr(:call, :+, p1.ex, p2.ex))
 end
--(p1::Phase, p2::Phase) = p1 + (-p2)
--(p1::Phase, p2) = p1 + (-p2)
--(p1, p2::Phase) = p1 + (-p2)
++(p1::Phase, p2::Number) = p1 + Phase(p2)
++(p1::Number, p2::Phase) = Phase(p1) + p2
 
-function *(p1::Phase, p2::Real)
-    if isnothing(p1.ex)
-        return Phase(p1.val * p2)
-    else
-        return Phase(:($(p1.ex) * $p2), p1.val * p2)
+function -(p1::Phase{T1}, p2::Phase{T2}) where {T1, T2}
+    if p1.ex isa Number && p2.ex isa Number
+        return Phase(p1.ex - p2.ex)
     end
+
+    T = Base.promote_op(-, T1, T2)
+    return Phase{T}(Expr(:call, :-, p1.ex, p2.ex))
 end
-function *(p1::Real, p2::Phase)
-    if isnothing(p2.ex)
-        return Phase(p2.val * p1)
-    else
-        return Phase(:($(p2.ex) * $p1), p2.val * p1)
+-(p1::Phase, p2::Number) = p1 - Phase(p2)
+-(p1::Number, p2::Phase) = Phase(p1) - p2
+
+function -(p::Phase{T0}) where {T0}
+    if p.ex isa Number
+        return Phase(-p.ex)
     end
+
+    T = Base.promote_op(-, T0)
+    return Phase{T}(Expr(:call, :-, p.ex))
 end
 
-==(p1::Phase, p2::Phase) = p1.ex == p2.ex && p1.val == p2.val
-==(p1::Phase, p2::Real) = isnothing(p1.ex) && p1.val == p2
-==(p1::Real, p2::Phase) = isnothing(p2.ex) && p2.val == p1
+function *(p1::Phase{T1}, p2::Phase{T2}) where {T1, T2}
+    if p1.ex isa Number && p2.ex isa Number
+        return Phase(p1.ex * p2.ex)
+    end
 
-isless(p1::Phase, p2::Real) = isnothing(p1.ex) && p1.val < p2
-rem(p::Phase, d::Real) = Phase(p.ex, rem(p.val, d))
+    T = Base.promote_op(*, T1, T2)
+    return Phase{T}(Expr(:call, :*, p1.ex, p2.ex))
+end
+*(p1::Phase, p2::Number) = p1 * Phase(p2)
+*(p1::Number, p2::Phase) = Phase(p1) * p2
+
+function /(p1::Phase{T1}, p2::Phase{T2}) where {T1, T2}
+    if p1.ex isa Number && p2.ex isa Number
+        return Phase(p1.ex / p2.ex)
+    end
+
+    T = Base.promote_op(/, T1, T2)
+    return Phase{T}(Expr(:call, :/, p1.ex, p2.ex))
+end
+/(p1::Phase, p2::Number) = p1 / Phase(p2)
+/(p1::Number, p2::Phase) = Phase(p1) / p2
+
+==(p1::Phase, p2::Phase) = p1.ex == p2.ex
+==(p1::Phase, p2::Number) = (p1 == Phase(p2))
+==(p1::Number, p2::Phase) = (Phase(p1) == p2)
+
+isless(p1::Phase, p2::Number) = (p1.ex isa Number) && p1.ex < p2
+function rem(p::Phase, d::Number)
+    if p.ex isa Number
+        return Phase(rem(p.ex, d))
+    end
+    return p
+end
 
 convert(::Type{Phase}, p) = Phase(p)
 convert(::Type{Phase}, p::Phase) = p
@@ -76,6 +90,6 @@ zero(::Type{Phase}) = Phase(0//1)
 one(::Phase) = Phase(1//1)
 one(::Type{Phase}) = Phase(1//1)
 
-iseven(p::Phase) = isnothing(p.ex) && (-1)^p.val > 0
+iseven(p::Phase) = (p.ex isa Number) && (-1)^p.ex > 0
 
 # p1+p2+p1-p1-p1
