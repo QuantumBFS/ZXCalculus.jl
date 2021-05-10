0ef28ea0e5faad1c8267cb69b40f23db97f83bc2
Rogerluo <rogerluo.rl18@gmail.com>
2021-05-09 23:52:18 -0400

    use YaoHIR (#71)
    * ignore vscode
    
    * use YaoHIR
    
    * test on latest stable
    
    * add compat
    
    * mark whatever is broken
    
    * fix HIR
    
    * update to latest YaoHIR
    
    * rm deprecated circuit
    
    * fix test

--- src/ZXCalculus.jl
+++ src/ZXCalculus.jl
@@ -1,15 +1,23 @@
 module ZXCalculus
 
+using YaoHIR
+using MLStyle
+using YaoLocations
+using YaoHIR: X, Y, Z, H, S, T, Rx, Ry, Rz, UGate, shift
+using YaoLocations: plain
+
+export convert_to_block_ir, convert_to_zxd
+
 include("phase.jl")
 include("scalar.jl")
 include("abstract_zx_diagram.jl")
 include("zx_layout.jl")
 include("zx_diagram.jl")
-include("qcircuit.jl")
 include("zx_graph.jl")
 include("rules.jl")
-
 include("simplify.jl")
+
+include("ir.jl")
 
 include("circuit_extraction.jl")
 include("phase_teleportation.jl")
