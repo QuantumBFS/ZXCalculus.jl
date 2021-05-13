251ccbad4070a866837312b529ea557ba2c90586
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2021-05-13 22:48:26 +0800

    simplification methods for YaoHIR

--- src/simplify.jl
+++ src/simplify.jl
@@ -38,8 +38,7 @@ Simplify `zxd` with the algorithms in [arXiv:1902.03178](https://arxiv.org/abs/1
 function clifford_simplification(circ::ZXDiagram)
     zxg = ZXGraph(circ)
     zxg = clifford_simplification(zxg)
-
-    return circuit_extraction(zxg)
+    return zxg
 end
 
 function clifford_simplification(zxg::ZXGraph)
@@ -50,11 +49,17 @@ function clifford_simplification(zxg::ZXGraph)
     return zxg
 end
 
+function clifford_simplification(bir::BlockIR)
+    zxd = convert_to_zxd(bir)
+    zxg = clifford_simplification(zxd)
+    chain = circuit_extraction(zxg)
+    return BlockIR(bir.parent, bir.nqubits, chain)
+end
+
 function full_reduction(cir::ZXDiagram)
     zxg = ZXGraph(cir)
     zxg = full_reduction(zxg)
-
-    return circuit_extraction(zxg)
+    return zxg
 end
 
 function full_reduction(zxg::ZXGraph)
@@ -80,118 +85,116 @@ function full_reduction(zxg::ZXGraph)
     return zxg
 end
 
-function bring_swap_forward!(qc::QCircuit)
-    qc_swap = QCircuit(nqubits(qc))
-    for i = gate_count(qc):-1:1
-        g = qc.gates[i]
-        if g.name === :SWAP
-            push_gate!(qc_swap, g)
-            loc1 = g.loc
-            loc2 = g.ctrl
-            loc_map = Dict{Int, Int}(loc1 => loc2, loc2 => loc1)
+function full_reduction(bir::BlockIR)
+    zxd = convert_to_zxd(bir)
+    zxg = full_reduction(zxd)
+    chain = circuit_extraction(zxg)
+    return BlockIR(bir.parent, bir.nqubits, chain)
+end
 
-            # delete g
-            deleteat!(qc, i)
-            
-            # change previous gates
-            for j in (i-1):-1:1
-                pg = qc.gates[j]
-                if pg.loc in (loc1, loc2)
-                    pg.loc = loc_map[pg.loc]
-                end
-                if pg.ctrl in (loc1, loc2)
-                    pg.ctrl = loc_map[pg.ctrl]
-                end
-            end
+function compose_permutation(p1::Dict{Int, Int}, p2::Dict{Int, Int})
+    p2 = copy(p2)
+    p = Dict{Int, Int}()
+    for (k1, v1) in p1
+        if haskey(p2, v1)
+            p[k1] = p2[v1]
+            delete!(p2, v1)
+        else
+            p[k1] = v1
         end
     end
-    qc.gates = [qc_swap.gates; qc.gates]
+    for (k2, v2) in p2
+        p[k2] = v2
+    end
+    return p
+end
+
+map_locations(d::Dict{T, T}, g::Gate) where {T <: Integer} = 
+    Gate(g.operation, map_locations(d, g.locations))
+map_locations(d::Dict{T, T}, cg::Ctrl) where {T <: Integer} = 
+    Ctrl(map_locations(d, cg.gate), map_locations(d, cg.ctrl))
+map_locations(d::Dict{T, T}, locs::Locations) where {T <: Integer} =
+    Locations(Tuple(haskey(d, i) ? d[i] : i for i in locs.storage))
+map_locations(d::Dict{T, T}, locs::CtrlLocations) where {T <: Integer} =
+    CtrlLocations(map_locations(d, locs.storage))
+
+function simplify_swap!(qc::Chain; replace_swap::Bool = true)
+    chain_after_swap = Chain()
+    loc_map = Dict{Int, Int}()
+    while length(qc.args) > 0
+        g = pop!(qc.args)
+        if g isa Gate
+            if g.operation === SWAP
+                loc1, loc2 = g.locations.storage[1:2]
+                loc_map = compose_permutation(Dict{Int, Int}(loc1 => loc2, loc2 => loc1), loc_map)
+                continue
+            end
+        end
+        pushfirst!(chain_after_swap.args, map_locations(loc_map, g))
+    end
+    chain_swap = generate_swap(loc_map)
+    replace_swap && replace_swap!(chain_swap, chain_after_swap)
+    for g in chain_swap.args
+        push!(qc.args, g)
+    end
+    for g in chain_after_swap.args
+        push!(qc.args, g)
+    end
     return qc
 end
 
-function simplify_swap!(qc::QCircuit)
-    i1 = 1
-    i2 = 0
-    for i = 1:gate_count(qc)
-        if qc.gates[i].name === :SWAP
-            i2 = i
-        else
-            break
-        end
-    end
-    qc_swap = qc[i1:i2]
-    deleteat!(qc, i1:i2)
-    perm = collect(1:nqubits(qc_swap))
-    for i = gate_count(qc_swap):-1:1
-        g = qc_swap.gates[i]
-        loc1 = g.loc
-        loc2 = g.ctrl
-        temp = perm[loc1]
-        perm[loc1] = perm[loc2]
-        perm[loc2] = temp
-    end
-
-    rec = collect(1:nqubits(qc_swap))
+function generate_swap(loc_map::Dict{T, T}) where {T <: Integer}
+    perm = copy(loc_map)
     subperms = []
-    while length(rec) > 0
-        id = rec[1]
-        sp = Int[]
-        while !(id in sp)
-            push!(sp, id)
-            deleteat!(rec, findfirst(isequal(id), rec))
-            id = perm[id]
+    while length(perm) > 0
+        k, v = pop!(perm)
+        k == v && continue
+        sp = [k]
+        while !(v in sp)
+            push!(sp, v)
+            v = pop!(perm, v)
         end
         push!(subperms, sp)
     end
 
-    qc_swap_opt = QCircuit(nqubits(qc_swap))
+    qc_swap = []
     for subperm in subperms
         for k in 2:length(subperm)
-            push_gate!(qc_swap_opt, Val(:SWAP), subperm[1], subperm[k])
+            push!(qc_swap, Gate(SWAP, Locations((subperm[1], subperm[k]))))
         end
     end
-    qc.gates = [qc_swap_opt.gates; qc.gates] 
-    return qc
+    return Chain(qc_swap...)
 end
 
-function replace_swap!(qc::QCircuit)
-    i1 = 1
-    i2 = 0
-    for i = 1:gate_count(qc)
-        if qc.gates[i].name === :SWAP
-            i2 = i
-        else
-            break
-        end
-    end
-    qc_swap = qc[i1:i2]
-    deleteat!(qc, i1:i2)
-
-    for i = gate_count(qc_swap):-1:1
-        g_swap = qc_swap.gates[i]
-        qmap = Dict(g_swap.loc => g_swap.ctrl, g_swap.ctrl => g_swap.loc)
+function replace_swap!(chain_swap::Chain, chain_after_swap::Chain)
+    qc_swap = chain_swap.args
+    qc_after_swap = chain_after_swap.args
+    while length(qc_swap) > 0
+        g_swap = pop!(qc_swap)
+        loc1, loc2 = g_swap.locations.storage[1:2]
+        qmap = Dict(loc1 => loc2, loc2 => loc1)
         j = 1
-        while j <= gate_count(qc)
-            g = qc.gates[j]
-            if g.name === :CNOT && haskey(qmap, g.loc) && haskey(qmap, g.ctrl)
-                insert!(qc, j+1, QGate(Val(:CNOT), g.ctrl, g.loc))
-                break
+        while j <= length(qc_after_swap)
+            g = qc_after_swap[j]
+            j += 1
+            g isa Ctrl || continue
+            if g.gate === X && length(g.gate.locations) == 1 && length(g.ctrl) == 1
+                loc = g.gate.locations.storage[]
+                ctrl = g.ctrl.storage.storage[]
+                if haskey(qmap, loc) && haskey(qmap, ctrl)
+                    insert!(qc, j, convert_to_gate(Val(:CNOT), ctrl, loc))
+                    break
+                end
             else
-                if haskey(qmap, g.loc)
-                    g.loc = qmap[g.loc]
-                end
-                if haskey(qmap, g.ctrl)
-                    g.ctrl = qmap[g.ctrl]
-                end
-                j += 1
+                qc_after_swap[j-1] = map_locations(qmap, qc_after_swap[j-1])
             end
         end
-        if j > gate_count(qc)
-            push_gate!(qc, Val(:CNOT), g_swap.loc, g_swap.ctrl)
-            push_gate!(qc, Val(:CNOT), g_swap.ctrl, g_swap.loc)
-            push_gate!(qc, Val(:CNOT), g_swap.loc, g_swap.ctrl)
+        if j > length(qc_after_swap)
+            push!(qc_after_swap, convert_to_gate(Val(:CNOT), loc1, loc2))
+            push!(qc_after_swap, convert_to_gate(Val(:CNOT), loc2, loc1))
+            push!(qc_after_swap, convert_to_gate(Val(:CNOT), loc1, loc2))
         end
     end
 
-    return qc
+    return chain_swap, chain_after_swap
 end
