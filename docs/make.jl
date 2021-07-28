4c6415d08a1a47a3dd3ac8828ed2a50e52ac50dc
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2021-07-28 22:11:51 +0800

    update doc theme

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,18 @@
-using Documenter, ZXCalculus
+using Documenter
+using DocThemeIndigo
+
+using ZXCalculus
 using Multigraphs
 
+indigo = DocThemeIndigo.install(ZXCalculus)
 makedocs(;
     modules = [ZXCalculus],
-    format = Documenter.HTML(prettyurls = !("local" in ARGS)),
+    format=Documenter.HTML(;
+        # ...
+        # put your indigo css in assets
+        assets=String[indigo #= your other assets =#],
+        prettyurls = !("local" in ARGS)
+    ),
     pages = ["Home" => "index.md",
         "Tutorials" => "tutorials.md",
         "Examples" => "examples.md",
