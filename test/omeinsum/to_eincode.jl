1b71b4341bf2c3b58fc7b1e01d9e5570517635d9
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2022-06-11 21:51:34 +0800

    improve codecov

--- test/omeinsum/to_eincode.jl
+++ test/omeinsum/to_eincode.jl
@@ -4,7 +4,7 @@ using Graphs
 using OMEinsum
 using LinearAlgebra
 
-using ZXCalculus: h_tensor, w_tensor, d_tensor
+using ZXCalculus: SpiderType, to_eincode_tensor, h_tensor, w_tensor, d_tensor
 
 zxd = ZXDiagram(2)
 push_gate!(zxd, Val(:CNOT), 1, 2)
@@ -14,8 +14,8 @@ ec, ts = ZXCalculus.to_eincode(zxd)
 reshape(ec(ts...), (4, 4))
 @test Matrix(zxd) ≈ diagm([1,1,1,1])
 
-@test_throws ErrorException h_tensor(3)
-@test_throws ErrorException d_tensor(3)
-@test w_tensor(2) ≈ [0 1; 1 0]
-@test h_tensor(2)^2 ≈ [1 0; 0 1]
-@test [0 1; 1 0] * d_tensor(2) ≈ [1 1; 0 1]
+@test_throws ErrorException to_eincode_tensor(SpiderType.H, 3, 0)
+@test_throws ErrorException to_eincode_tensor(SpiderType.D, 1, 0)
+@test to_eincode_tensor(SpiderType.W, 2, 0) ≈ [0 1; 1 0]
+@test to_eincode_tensor(SpiderType.H, 2, 0)^2 ≈ [1 0; 0 1]
+@test [0 1; 1 0] * to_eincode_tensor(SpiderType.D, 2, 0) ≈ [1 1; 0 1]
