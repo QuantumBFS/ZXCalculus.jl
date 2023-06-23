527d300db4b8156da7080f32720ca4daa359db76
Yusheng Zhao <yushengzhao2020@outlook.com>
2023-06-23 22:07:22 +0800

    implement + and its test

--- src/parameter.jl
+++ src/parameter.jl
@@ -21,10 +21,10 @@ end
    Parameter
 Universal constructor for ADT
 """
-function Parameter(x, type::String = "Factor")
+function Parameter(x, type::String="Factor")
     @match (x, type) begin
-        if type == "PiUnit" end => PiUnit(x,typeof(x))
-        (f :: Number, "Factor") => Factor(f)
+        if type == "PiUnit" end => PiUnit(x, typeof(x))
+        (f::Number, "Factor") => Factor(f)
         (PiUnit(_...), _) => PiUnit(x.pu, x.pu_type)
         (Factor(_...), _) => Factor(x.f)
         _ => error("Invalid input '$(x)' of type $(typeof(x)) for ADT: $(type)")
@@ -34,8 +34,28 @@ end
 
 function Base.show(io::IO, p::Parameter)
     @match p begin
-        PiUnit(_...) && if p.pu isa Number end => print(io, "$(p.pu)⋅π")
-        PiUnit(_...) && if !(p.pu isa Number) end => print(io, "PiUnit(($(p.pu))::$(p.pu_type))")
-        Factor(_...) => print(io, "$(p.f)")
+        PiUnit(pu, _) && if pu isa Number
+        end => print(io, "$(pu)⋅π")
+        PiUnit(pu, pu_type) && if !(pu isa Number)
+        end => print(io, "PiUnit(($(pu))::$(pu_type))")
+        Factor(f) => print(io, "$(f)")
     end
 end
+
+function add_param(p1, p2)
+    @match (p1, p2) begin
+        (PiUnit(pu1, pu_t1), PiUnit(pu2, pu_t2)) && if pu1 isa Number && pu2 isa Number end => PiUnit(pu1 + pu2, pu_t1)
+        (PiUnit(pu1, pu_t1), PiUnit(pu2, pu_t2)) && if !(pu1 isa Number) ||  !(pu2 isa Number) end => PiUnit(Expr(:call, :+, pu1, pu2), Expr)
+        (Factor(f1), Factor(f2)) => Factor(f1 + f2)
+        (PiUnit(_...), Factor(_...)) => Parameter(exp(im*p1.pu*π)+p2, "Factor")
+        (Factor(_...), PiUnit(_...)) => Parameter(exp(im*p2.pu*π)+p1, "Factor")
+        (PiUnit(_...), _ ) => p1 + Parameter(p2, "PiUnit")
+        (Factor(_...), ::Number) => p1 + Parameter(p2, "Factor")
+        _ => error("Invalid input '$(p1)' of type $(typeof(p1)) and '$(p2)' of type $(typeof(p2)) for ADT: +")
+
+    end
+end
+
+Base.:(+)(p1::Parameter, p2::Parameter) = add_param(p1, p2)
+Base.:(+)(p1::Parameter, p2::Number) = add_param(p1, p2)
+Base.:(+)(p1::Number, p2::Parameter) = add_param(p2, p1)
--- test/parameter.jl
+++ test/parameter.jl
@@ -21,6 +21,12 @@
     p5 = Parameter(:a, "PiUnit")
     @test p5.pu == :a && p5.pu_type == Symbol
 
+    p6 = Parameter(Expr(:call, :+, 1.0, :a), "PiUnit")
+    @test p6.pu == Expr(:call, :+, 1.0, :a) && p6.pu_type == Expr
+
+    p7 = Parameter(Expr(:call, :+, :a, :b), "PiUnit")
+    @test p7.pu == Expr(:call, :+, :a, :b) && p7.pu_type == Expr
+
     f4 = Parameter(f3)
     @test f4.f == f3.f
 
@@ -28,7 +34,7 @@
     @test_throws ErrorException Parameter(:a, "Factor")
 end
 
-@testset "utilities" begin
+@testset "io" begin
 
     io = IOBuffer()
     p1 = Parameter(1.0, "PiUnit")
@@ -42,4 +48,26 @@ end
     f1 = Parameter(1, "Factor")
     show(io, f1)
     @test String(take!(io)) == "1"
+end
+
+@testset "addition" begin
+    p1 = Parameter(1.0, "PiUnit")
+    p2 = Parameter(2, "PiUnit")
+    p3 = Parameter(:a, "PiUnit")
+    p4 = Parameter(:b, "PiUnit")
+    f1 = Parameter(1, "Factor")
+    f2 = Parameter(2.5, "Factor")
+
+    @test p1 + p2 == Parameter(3.0, "PiUnit")
+    @test p1 + 2 == Parameter(3.0, "PiUnit")
+    @test 2 + p1 == Parameter(3.0, "PiUnit")
+    @test 2 + f2 == Parameter(4.5, "Factor")
+    @test f2 + 2 == Parameter(4.5, "Factor")
+    p13 = p1 + p3
+    @test p13.pu == Expr(:call, :+, 1.0, :a) && p13.pu_type == Expr
+    p34 = p3 + p4
+    @test p34.pu == Expr(:call, :+, :a, :b) && p34.pu_type == Expr
+
+    @test p2 + f1 == Parameter(exp(im * 2 * π) + 1, "Factor")
+
 end
