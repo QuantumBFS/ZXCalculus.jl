1f5787b1e03d4635d70967f85eab5545fc1efa30
Yusheng Zhao <yushengzhao2020@outlook.com>
2023-07-01 17:08:11 +0800

    temporarily remove CompilerPluginTools tests

--- test/phase.jl
+++ test/phase.jl
@@ -1,29 +1,29 @@
 using Test
 using YaoHIR
 using YaoLocations
-using CompilerPluginTools
+# using CompilerPluginTools
 using ZXCalculus
 using ZXCalculus: Phase, BlockIR
 using YaoHIR.IntrinsicOperation
 using YaoHIR: Chain, Gate, Ctrl
 
-ir = @make_ircode begin
-    Expr(:call, :+, 1, 1)::Int
-    Expr(:call, :+, 3, 3)::Int
-end
+# ir = @make_ircode begin
+#     Expr(:call, :+, 1, 1)::Int
+#     Expr(:call, :+, 3, 3)::Int
+# end
 
-circ = Chain(
-    Gate(shift(1.0), Locations(1)),
-    Gate(Rz(Core.SSAValue(2)), Locations(1)),
-)
+# circ = Chain(
+#     Gate(shift(1.0), Locations(1)),
+#     Gate(Rz(Core.SSAValue(2)), Locations(1)),
+# )
 
-bir = BlockIR(ir, 4, circ)
+# bir = BlockIR(ir, 4, circ)
 
-zxd = convert_to_zxd(bir)
-c = clifford_simplification(zxd)
-ZXCalculus.generate_layout!(zxd)
-qc_tl = convert_to_chain(phase_teleportation(zxd))
-@test length(qc_tl) == 1
+# zxd = convert_to_zxd(bir)
+# c = clifford_simplification(zxd)
+# ZXCalculus.generate_layout!(zxd)
+# qc_tl = convert_to_chain(phase_teleportation(zxd))
+# @test length(qc_tl) == 1
 p = Phase(1//1)
 @test p + 1 == 1 + p == p + p
 @test p - 1 == 1 - p == p - p
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,65 +3,65 @@ using YaoHIR: Chain
 using Documenter
 using Test
 
-# @testset "parameter.jl" begin
-#     include("parameter.jl")
-# end
+@testset "parameter.jl" begin
+    include("parameter.jl")
+end
 
-# @testset "zxw_diagram.jl" begin
-#     include("zxw_diagram.jl")
-# end
+@testset "zxw_diagram.jl" begin
+    include("zxw_diagram.jl")
+end
 
-# @testset "utils.jl" begin
-#     include("utils.jl")
-# end
+@testset "utils.jl" begin
+    include("utils.jl")
+end
 
 @testset "to_eincode.jl" begin
     include("to_eincode.jl")
 end
-#
-# @testset "scalar.jl" begin
-#     include("scalar.jl")
-# end
 
-# @testset "abstract_zx_diagram.jl" begin
-#     include("abstract_zx_diagram.jl")
-# end
+@testset "scalar.jl" begin
+    include("scalar.jl")
+end
 
-# @testset "zx_diagram.jl" begin
-#     include("zx_diagram.jl")
-# end
+@testset "abstract_zx_diagram.jl" begin
+    include("abstract_zx_diagram.jl")
+end
 
-# @testset "rules.jl" begin
-#     include("rules.jl")
-# end
+@testset "zx_diagram.jl" begin
+    include("zx_diagram.jl")
+end
 
-# @testset "zx_graph.jl" begin
-#     include("zx_graph.jl")
-# end
+@testset "rules.jl" begin
+    include("rules.jl")
+end
 
-# @testset "circuit_extraction.jl" begin
-#     include("circuit_extraction.jl")
-# end
+@testset "zx_graph.jl" begin
+    include("zx_graph.jl")
+end
 
-# @testset "phase_teleportation.jl" begin
-#     include("phase_teleportation.jl")
-# end
+@testset "circuit_extraction.jl" begin
+    include("circuit_extraction.jl")
+end
+
+@testset "phase_teleportation.jl" begin
+    include("phase_teleportation.jl")
+end
 
 # @testset "ir.jl" begin
 #     include("ir.jl")
 # end
 
-# @testset "phase.jl" begin
-#     include("phase.jl")
-# end
+@testset "phase.jl" begin
+    include("phase.jl")
+end
 
-# @testset "simplify.jl" begin
-#     include("simplify.jl")
-# end
+@testset "simplify.jl" begin
+    include("simplify.jl")
+end
 
-# @testset "ancilla_extraction.jl" begin
-#     include("ancilla_extraction.jl")
-#     include("challenge.jl")
-# end
+@testset "ancilla_extraction.jl" begin
+    include("ancilla_extraction.jl")
+    include("challenge.jl")
+end
 
-#  doctest(ZXCalculus)
+ doctest(ZXCalculus)
