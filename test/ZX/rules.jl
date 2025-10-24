e2a3ae5474e88c91458b5f163f47f169d243e4ef
Chen Zhao <czhao.cz44@gmail.com>
2025-10-23 21:02:36 -0400

    polish test

--- test/ZX/rules.jl
+++ test/ZX/rules.jl
@@ -2,41 +2,42 @@ using Test
 using ZXCalculus, Multigraphs, ZXCalculus.ZX, ZXCalculus.Utils, Graphs
 using ZXCalculus: ZX
 using ZXCalculus.Utils: Phase
-using Vega, DataFrames
 
 @testset "FusionRule" begin
-    g = Multigraph([0 2 0; 2 0 1; 0 1 0])
-    collect(edges(g))
-    ps = [Phase(i // 4) for i in 1:3]
-    v_t = [SpiderType.Z, SpiderType.Z, SpiderType.X]
-    zxd = ZXDiagram(g, v_t, ps)
-    matches = match(FusionRule(), zxd)
-    rewrite!(FusionRule(), zxd, matches)
-    @test sort!(spiders(zxd)) == [1, 3]
-    @test phase(zxd, 1) == phase(zxd, 3) == 3 // 4
-    @test !isnothing(zxd)
+    @testset "ZXDiagram" begin
+        g = Multigraph([0 2 0; 2 0 1; 0 1 0])
+        collect(edges(g))
+        ps = [Phase(i // 4) for i in 1:3]
+        v_t = [SpiderType.Z, SpiderType.Z, SpiderType.X]
+        zxd = ZXDiagram(g, v_t, ps)
+        matches = match(FusionRule(), zxd)
+        rewrite!(FusionRule(), zxd, matches)
+        @test sort!(spiders(zxd)) == [1, 3]
+        @test phase(zxd, 1) == phase(zxd, 3) == 3 // 4
+        @test !isnothing(zxd)
+    end
 
-    zxg = ZXGraph()
-    v1 = ZX.add_spider!(zxg, SpiderType.In)
-    v2 = ZX.add_spider!(zxg, SpiderType.Out)
-    v3 = ZX.add_spider!(zxg, SpiderType.In)
-    v4 = ZX.add_spider!(zxg, SpiderType.Out)
-    v5 = ZX.add_spider!(zxg, SpiderType.Z, Phase(0//1), [v1])
-    v6 = ZX.add_spider!(zxg, SpiderType.Z, Phase(0//1), [v2])
-    v7 = ZX.add_spider!(zxg, SpiderType.Z, Phase(0//1), [v3, v4])
-    v8 = ZX.add_spider!(zxg, SpiderType.X, Phase(0//1))
-    ZX.add_edge!(zxg, v5, v6, EdgeType.SIM)
-    ZX.add_edge!(zxg, v5, v7, EdgeType.HAD)
-    ZX.add_edge!(zxg, v6, v7, EdgeType.HAD)
-    ZX.add_edge!(zxg, v7, v8, EdgeType.SIM)
-    plot(zxg)
-    ZX.add_edge!(zxg, v7, v8, EdgeType.SIM)
-    @test zxg.scalar == Scalar(-2, 0 // 1)
-    matches = match(FusionRule(), zxg)
-    plot(zxg)
-    rewrite!(FusionRule(), zxg, matches)
-    plot(zxg)
-    @test zxg.scalar == Scalar(-4, 0 // 1)
+    @testset "ZXGraph" begin
+        zxg = ZXGraph()
+        v1 = ZX.add_spider!(zxg, SpiderType.In)
+        v2 = ZX.add_spider!(zxg, SpiderType.Out)
+        v3 = ZX.add_spider!(zxg, SpiderType.In)
+        v4 = ZX.add_spider!(zxg, SpiderType.Out)
+        v5 = ZX.add_spider!(zxg, SpiderType.Z, Phase(0//1), [v1])
+        v6 = ZX.add_spider!(zxg, SpiderType.Z, Phase(0//1), [v2])
+        v7 = ZX.add_spider!(zxg, SpiderType.Z, Phase(0//1), [v3, v4])
+        v8 = ZX.add_spider!(zxg, SpiderType.X, Phase(0//1))
+        ZX.add_edge!(zxg, v5, v6, EdgeType.SIM)
+        ZX.add_edge!(zxg, v5, v7, EdgeType.HAD)
+        ZX.add_edge!(zxg, v6, v7, EdgeType.HAD)
+        ZX.add_edge!(zxg, v7, v8, EdgeType.SIM)
+        ZX.add_edge!(zxg, v7, v8, EdgeType.SIM)
+        @test zxg.scalar == Scalar(-2, 0 // 1)
+        matches = match(FusionRule(), zxg)
+        rewrite!(FusionRule(), zxg, matches)
+        @test zxg.scalar == Scalar(-4, 0 // 1)
+        @test nv(zxg) == 7 && ne(zxg) == 4
+    end
 end
 
 @testset "Identity1Rule" begin
@@ -51,13 +52,13 @@ end
     @test !isnothing(zxd)
 end
 
-@testset "HadamardRule and Identity2Rule" begin
+@testset "XToZRule and Identity2Rule" begin
     g = Multigraph([0 2 0; 2 0 1; 0 1 0])
     ps = [Phase(i // 4) for i in 1:3]
     v_t = [SpiderType.X, SpiderType.X, SpiderType.Z]
     zxd = ZXDiagram(g, v_t, ps)
-    matches = match(HadamardRule(), zxd)
-    rewrite!(HadamardRule(), zxd, matches)
+    matches = match(XToZRule(), zxd)
+    rewrite!(XToZRule(), zxd, matches)
     @test nv(zxd) == 8 && ne(zxd) == 8
     @test !isnothing(zxd)
 
