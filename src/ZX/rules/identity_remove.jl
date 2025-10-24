97bd534229a22b4cf1a20ee84bb1302239716cab
Chen Zhao <czhao.cz44@gmail.com>
2025-10-24 14:32:20 -0400

    identity removal for ZXCircuit

--- src/ZX/rules/identity_remove.jl
+++ src/ZX/rules/identity_remove.jl
@@ -7,28 +7,18 @@ function Base.match(::IdentityRemovalRule, zxg::ZXGraph{T, P}) where {T, P}
         if spider_type(zxg, v2) == SpiderType.Z && length(nb2) == 2
             v1, v3 = nb2
             if is_zero_phase(phase(zxg, v2))
-                if spider_type(zxg, v1) == SpiderType.Z && spider_type(zxg, v3) == SpiderType.Z
+                if spider_type(zxg, v1) == spider_type(zxg, v3) == SpiderType.Z
+                    push!(matches, Match{T}([v1, v2, v3]))
+                elseif (spider_type(zxg, v1) in (SpiderType.In, SpiderType.Out)) &&
+                       (spider_type(zxg, v3) in (SpiderType.In, SpiderType.Out))
                     push!(matches, Match{T}([v1, v2, v3]))
                 end
-
-                if (
-                    (
-                    spider_type(zxg, v1) == SpiderType.In ||
-                    spider_type(zxg, v1) == SpiderType.Out
-                ) && (
-                    spider_type(zxg, v3) == SpiderType.In ||
-                    spider_type(zxg, v3) == SpiderType.Out
-                )
-                )
-                    push!(matches, Match{T}([v1, v2, v3]))
-                end
-            else
-                is_one_phase(phase(zxg, v2))
-                if spider_type(zxg, v1) == SpiderType.Z && spider_type(zxg, v3) == SpiderType.Z
+            elseif is_one_phase(phase(zxg, v2))
+                if spider_type(zxg, v1) == spider_type(zxg, v3) == SpiderType.Z
                     if degree(zxg, v1) == 1
                         push!(matches, Match{T}([v1, v2, v3]))
                     elseif degree(zxg, v3) == 1
-                        push!(matches, Match{T}([v1, v2, v3]))
+                        push!(matches, Match{T}([v3, v2, v1]))
                     end
                 end
             end
@@ -39,23 +29,23 @@ end
 
 function check_rule(::IdentityRemovalRule, zxg::ZXGraph{T, P}, vs::Vector{T}) where {T, P}
     v1, v2, v3 = vs
-    if has_vertex(zxg.mg, v2)
+    if has_vertex(zxg, v2)
         nb2 = neighbors(zxg, v2)
         if spider_type(zxg, v2) == SpiderType.Z && length(nb2) == 2
             (v1 in nb2 && v3 in nb2) || return false
             if is_zero_phase(phase(zxg, v2))
-                if spider_type(zxg, v1) == SpiderType.Z && spider_type(zxg, v3) == SpiderType.Z
+                if spider_type(zxg, v1) == spider_type(zxg, v3) == SpiderType.Z
                     return true
                 end
-                if ((spider_type(zxg, v1) == SpiderType.In || spider_type(zxg, v1) == SpiderType.Out) &&
-                    (spider_type(zxg, v3) == SpiderType.In || spider_type(zxg, v3) == SpiderType.Out))
+                if (spider_type(zxg, v1) in (SpiderType.In, SpiderType.Out)) &&
+                   (spider_type(zxg, v3) in (SpiderType.In, SpiderType.Out))
                     return true
                 end
 
             else
                 is_one_phase(phase(zxg, v2))
-                if spider_type(zxg, v1) == SpiderType.Z && spider_type(zxg, v3) == SpiderType.Z
-                    return degree(zxg, v1) == 1 || degree(zxg, v3) == 1
+                if spider_type(zxg, v1) == spider_type(zxg, v3) == SpiderType.Z
+                    return degree(zxg, v1) == 1
                 end
             end
         end
@@ -68,20 +58,13 @@ function rewrite!(::IdentityRemovalRule, zxg::ZXGraph{T, P}, vs::Vector{T}) wher
     if is_one_phase(phase(zxg, v2))
         set_phase!(zxg, v2, zero(P))
         set_phase!(zxg, v1, -phase(zxg, v1))
-        zxg.phase_ids[v1] = (zxg.phase_ids[v1][1], -zxg.phase_ids[v1][2])
     end
     if ((spider_type(zxg, v1) == SpiderType.In || spider_type(zxg, v1) == SpiderType.Out ||
          spider_type(zxg, v3) == SpiderType.In || spider_type(zxg, v3) == SpiderType.Out))
         rem_spider!(zxg, v2)
         add_edge!(zxg, v1, v3, EdgeType.SIM)
-
     else
-        # TODO: to ZXCircuit
-        # set_phase!(zxg, v3, phase(zxg, v3)+phase(zxg, v1))
-        # id1, mul1 = zxg.phase_ids[v1]
-        # id3, mul3 = zxg.phase_ids[v3]
-        # set_phase!(zxg.master, id3, (mul3 * phase(zxg.master, id3) + mul1 * phase(zxg.master, id1)) * mul3)
-        # set_phase!(zxg.master, id1, zero(P))
+        set_phase!(zxg, v3, phase(zxg, v3)+phase(zxg, v1))
         for v in neighbors(zxg, v1)
             v == v2 && continue
             add_edge!(zxg, v, v3, is_hadamard(zxg, v, v1) ? EdgeType.HAD : EdgeType.SIM)
@@ -89,4 +72,21 @@ function rewrite!(::IdentityRemovalRule, zxg::ZXGraph{T, P}, vs::Vector{T}) wher
         rem_spiders!(zxg, [v1, v2])
     end
     return zxg
+end
+
+function rewrite!(::IdentityRemovalRule, circ::ZXCircuit{T, P}, vs::Vector{T}) where {T, P}
+    v1, v2, v3 = vs
+    if is_one_phase(phase(circ, v2))
+        circ.phase_ids[v1] = (circ.phase_ids[v1][1], -circ.phase_ids[v1][2])
+    end
+
+    if spider_type(circ, v1) == spider_type(circ, v3) == SpiderType.Z
+        id1, mul1 = circ.phase_ids[v1]
+        id3, mul3 = circ.phase_ids[v3]
+        set_phase!(circ.master, id3, (mul3 * phase(circ.master, id3) + mul1 * phase(circ.master, id1)) * mul3)
+        set_phase!(circ.master, id1, zero(P))
+    end
+
+    rewrite!(IdentityRemovalRule(), circ.zx_graph, vs)
+    return circ
 end
